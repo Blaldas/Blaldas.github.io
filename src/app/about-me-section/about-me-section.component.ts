7b1248f5d786e264a91b64f6d8b0687489b95a7e
Blaldas <marmingues00@gmail.com>
2023-07-30 17:02:42 +0100

    updated about me text

--- src/app/about-me-section/about-me-section.component.ts
+++ src/app/about-me-section/about-me-section.component.ts
@@ -13,8 +13,11 @@ export class AboutMeSectionComponent implements OnInit {
   curly_open = ' {';
   curly_close = '}';
 
-  constructor() { 
-    this.sectionText = "Lorem ipsum dolor sit amet. Aut nemo voluptatibus nam Quis magnam eum Quis voluptatibus in placeat harum. Aut debitis nihil At soluta nulla sed officia soluta et quia inventore. Et praesentium quam ut obcaecati quis qui tempora dignissimos rem necessitatibus animi eos aliquid Quis qui dicta veniam et itaque iure. Et porro mollitia et nulla quia ut sunt quibusdam est magnam eligendi   Qui quaerat neque ad itaque eaque aut fugiat quidem est laboriosam deleniti."
+  constructor() {
+    this.sectionText = "I'm Marco Domingues, a programmer from Portugal, with a passion for turning complex challenges into simple solutions. With a rich background in the world of software development, I've honed my skills in various programming languages, frameworks, and technologies.\n" +
+      "As an objective and detail-oriented person, I embrace a systematic and strategic approach to my work. Every line of code I write is meticulously planned, tested, and refined to ensure top-notch performance and reliability. I take pride in my ability to break down intricate problems into manageable tasks, making progress with clarity and precision.\n" +
+      "As a firm believer in continuous learning, I am always open to exploring new challenges that further develop my skill set. For me, programming is not just a profession. It's a passion that fuels my drive to conquer even the most complex challenges.\n" +
+      "Whether you're a  programmer, a tech enthusiast, or an organization seeking innovation, I welcome you to explore my GitHub. I have both finished and ongoing projects that exemplify my expertise.\n";
     }
 
 
